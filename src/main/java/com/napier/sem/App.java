855f750908dea6923bc81d6aa50519c52396c55f
McPenquen <majda.calk@outlook.com>
2020-01-21 18:58:28 +0000

    Added JAR connection, Updated Dockerfile, update travis.yml

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,10 +1,35 @@
 package com.napier.sem;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
 public class App {
 
     public static void main(String[] args)
     {
+
+
         System.out.println("Boo yah!");
+
+        // Connect to MongoDB on local system - we're using port 27000
+        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        // Get a database - will create when we use it
+        MongoDatabase database = mongoClient.getDatabase("mydb");
+        // Get a collection from the database
+        MongoCollection<Document> collection = database.getCollection("test");
+        // Create a document to store
+        Document doc = new Document("name", "Kevin Chalmers")
+                .append("class", "Software Engineering Methods")
+                .append("year", "2018/19")
+                .append("result", new Document("CW", 95).append("EX", 85));
+        // Add document to collection
+        collection.insertOne(doc);
+
+        // Check document in collection
+        Document myDoc = collection.find().first();
+        System.out.println(myDoc.toJson());
+
     }
 
 }
