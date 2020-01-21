7b83bf4ebaa95bbbe8af0a58b29937e0422d5052
McPenquen <majda.calk@outlook.com>
2020-01-21 17:51:58 +0000

    Added Java App, Dockerfile, README.md file,

---
+++ README.md
@@ -0,0 +1,2 @@
+jhnunyh
+yeas
---
+++ src/main/java/com/napier/sem/App.java
@@ -0,0 +1,10 @@
+package com.napier.sem;
+
+public class App {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Boo yah!");
+    }
+
+}
