ce266cdaac147e4a46dbdbb674a3b80ccbb92edd
omonimus1 <davidepollicino2015@gmail.com>
2020-02-07 13:45:00 +0000

    Updated pom.xml

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:latest
-COPY ./target/seMethods-0.1.0.1-jar-with-dependencies.jar /tmp
+COPY ./target/seMethods-0.1.0.2-jar-with-dependencies.jar /tmp
 WORKDIR /tmp
 ENTRYPOINT ["java", "-jar", "seMethods-0.1.0.1-jar-with-dependencies.jar"]
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,30 +1,9 @@
 package com.napier.sem;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
-import org.bson.Document;
-
 public class App
 {
     public static void main(String[] args)
     {
-        // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
-        // Get a database - will create when we use it
-        MongoDatabase database = mongoClient.getDatabase("mydb");
-        // Get a collection from the database
-        MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
-        Document doc = new Document("name", "George Washington")
-                .append("class", "Presidenting Methods")
-                .append("year", "1789")
-                .append("result", new Document("CW", 75).append("EX", 90));
-        // Add document to collection
-        collection.insertOne(doc);
-
-        // Check document in collection
-        Document myDoc = collection.find().first();
-        System.out.println(myDoc.toJson());
+        System.out.println("Boo yah!");
     }
 }
